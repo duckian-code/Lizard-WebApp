bb315878fb40ca2229201e2b084b0dd212334de5
Lily <66748489+duckian-code@users.noreply.github.com>
2025-08-17 21:37:57 -0600

    Add Lizard Upgrades
    Add Lizard Upgrades with full gameplay functionality

--- js/main.js
+++ js/main.js
@@ -1,10 +1,68 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const lizardbutton = document.getElementById('lizard-button');
-	const countDisplay = document.getElementById('count')
-	let count = 0;
+let increment = 1;
+let addPointCost = 10;
+let multPointCost = 100;
+let autoLizardCost = 1000;
+// starting 500 ms ahead so the first auto is exactly 5
+let interval = 5500; // in ms, starts at 5
+let count = 0;
+let timer;
 
+const lizardbutton = document.getElementById('lizard-button');
+const countDisplay = document.getElementById('count');
+const addPointUpgBtn = document.getElementById('add-point-upgrade');
+const multiPointUpgBtn = document.getElementById('mult-point-upgrade');
+const autoLizardBtn = document.getElementById('auto-lizard');
+
+document.addEventListener('DOMContentLoaded', function () {
 	lizardbutton.addEventListener('click', function () {
-		count += 1;
-		countDisplay.innerHTML = count;
+		clickButton();
 	});
+
+	addPointUpgBtn.addEventListener('click', function() {
+		if (count >= addPointCost) {
+			increment += 1;
+			count -= addPointCost;
+			countDisplay.innerHTML = count;
+			addPointCost *= 2;
+			// querySelector selects p element within button
+			document.querySelector("#add-point-upgrade p").textContent = addPointCost;
+		}
+	});
+
+	multiPointUpgBtn.addEventListener('click', function() {
+		if (count >= multPointCost) {
+			increment *= 2;
+			count -= multPointCost;
+			countDisplay.innerHTML = count;
+			multPointCost *= 2;
+			document.querySelector("#mult-point-upgrade p").textContent = multPointCost;
+		}
+	});
+
+	autoLizardBtn.addEventListener('click', function() {
+		if (count >= autoLizardCost) {
+			count -= autoLizardCost;
+			autoLizardCost *= 2;
+			countDisplay.innerHTML = count;
+			// ensures the lowest the button can go is 200 ms
+			interval = Math.max(200, interval - 500);
+			startIncrement();
+			document.querySelector("#auto-lizard p").textContent = autoLizardCost;
+		}
+	})
 });
+
+function startIncrement() {
+	if (timer) {
+		clearInterval(timer);
+	}
+	timer = setInterval(() => {
+		clickButton();
+	}, interval);
+}
+
+function clickButton() {
+	count += increment;
+	countDisplay.innerHTML = count;
+}
+
